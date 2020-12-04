d3a27b61e0502e9b24311a6d613e6fabe6fcdef1
smainyounes <younes.abd.smain@gmail.com>
2020-12-04 02:50:21 +0100

    adding the slugify methode & editing the upload img method

--- backend/helpers/helpers.php
+++ backend/helpers/helpers.php
@@ -12,6 +12,33 @@
 		}
 	}
 
+	function slugify($text)
+	{
+	  // replace non letter or digits by -
+	  $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+	  // transliterate
+	  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+	  // remove unwanted characters
+	  $text = preg_replace('~[^-\w]+~', '', $text);
+
+	  // trim
+	  $text = trim($text, '-');
+
+	  // remove duplicate -
+	  $text = preg_replace('~-+~', '-', $text);
+
+	  // lowercase
+	  $text = strtolower($text);
+
+	  if (empty($text)) {
+	    return 'n-a';
+	  }
+
+	  return $text;
+	}
+
 
 	function DeletePic($link)
 	{
@@ -29,44 +56,6 @@
 	        $randomString .= $characters[rand(0, $charactersLength - 1)];
 	    }
 	    return $randomString;
-	}
-
-	function UploadPics()
-	{
-		$upname = "";
-		$realname = "";
-		$error = "";
-
-		// image mime to be checked 
-		$imagetype = array(image_type_to_mime_type(IMAGETYPE_GIF), image_type_to_mime_type(IMAGETYPE_JPEG),
-		    image_type_to_mime_type(IMAGETYPE_PNG), image_type_to_mime_type(IMAGETYPE_BMP));
-		
-		$FOLDER = "img/";
-		$myfile = $_FILES["imgprod"];
-		$keepName = false; // change this for file name.
-		for ($i = 0; $i < count($myfile["name"]); $i++) {
-		    if ($myfile["name"][$i] <> "" && $myfile["error"][$i] == 0) {
-		        // file is ok
-		        if (in_array($myfile["type"][$i], $imagetype)) {
-		            //Set file name
-		            if($keepName) {
-		                $file_name =  $myfile["name"][$i];
-		            } else {
-		                // get extention and set unique name
-		                $file_extention = @strtolower(@end(@explode(".", $myfile["name"][$i])));
-		                $file_name = date("Ymd") . '_' . rand(10000, 990000) . '.' . $file_extention;
-		            }
-		            if (!move_uploaded_file($myfile["tmp_name"][$i], $FOLDER . $file_name)) {
-		            	$error = "file not moved";
-		            }
-		        } else {
-		        	$error = "invalid file type";
-		        }
-		    }
-		    $all[] = array("filename"=> $myfile["name"][$i], "uploadedname"=> $file_name, "error"=> $error);
-		}		
-
-		return $all;
 	}
 
 	function compressImage($source, $destination, $quality = 90)
@@ -136,52 +125,39 @@
 				$file_name = $prefix."_". date("YmdHis") . rand(10000, 9999999) . ".";
 
 				if ($file['size'] > (8 * 1000 * 1000)) {
-					return array('status' => 'error', 'data' => ['msg' => 'file too big! max 8 MB']);
+					return ['status' => 'error', 'msg' => 'file too big! max 8 MB'];
 				}
 
-				if ($prefix !== "local") {
+				if ($prefix !== "cover" && $prefix !== "profile") {
 					// move file no need to compress or make preview
 					if (move_uploaded_file($file["tmp_name"], $dir . $file_name . $file_extention)) {
-						return array('status' => 'success', 'data' => ['filename' => $file_name . $file_extention]);
+						return ['status' => 'success', 'data' => ['filename' => $file_name . $file_extention]];
 					}else{
-						return array('status' => 'error', 'data' => ['msg' => 'file could not be moved']);
+						return ['status' => 'error', 'msg' => 'file could not be moved'];
 					}
 				}
 
-				$sub_quality = 20;
-				$quality = 90;
-
-
-				if ($file['size'] > (2 * 1000 * 1000)) {
-					$quality = 80;
-				}
-
-				if ($file['size'] > (5 * 1000 * 1000)) {
-					$quality = 50;
-					$sub_quality = 10;
-				}
-
 				// normal img
-				compressImage($file["tmp_name"], $dir . $file_name . "jpeg", $quality);
+				resizeImage($file["tmp_name"], $dir . $file_name . "jpeg", 1200, 1200);
 
 				// preview img
 				resizeImage($dir . $file_name . "jpeg", $dir . "preview/" . $file_name . "jpeg");
 
 				if (file_exists($dir . $file_name . "jpeg") && file_exists($dir ."preview/". $file_name . "jpeg")) {
 					// file been compressed
-					return array('status' => 'success', 'data' => ['filename' => $file_name . "jpeg"]);
+					return ['status' => 'success', 'data' => ['filename' => $file_name . "jpeg"]];
 				}else{
 					// file wasnt compressed
-					return array('status' => 'error', 'data' => ['msg' => 'file could not be compressed']);
+					return ['status' => 'error', 'msg' => 'file could not be compressed'];
 				}		
 
 			}else{
 				// file type not accepted
-				return array('status' => 'error', 'data' => ['msg' => 'file type not accepted']);
+				return ['status' => 'error', 'msg' => 'file type not accepted'];
 			}
 		}else{
 			// file didnt upload
-			return array('status' => 'error', 'data' => ['msg' => 'file could not be uploaded']);
+			return ['status' => 'error', 'msg' => 'file could not be uploaded'];
 		}
 	}
 
@@ -206,7 +182,54 @@
 
 	    $image_p = imagecreatetruecolor($width, $height);
 
-	    $image = imagecreatefromjpeg($filename);
+	    $info = getimagesize($filename);
+	    switch ($info['mime']) {
+	    	case 'image/jpeg':
+	    		$image = imagecreatefromjpeg($filename);
+	    		break;
+	    	case 'image/png':
+	    		$image = imagecreatefrompng($filename);
+	    		break;
+	    	case 'image/gif':
+	    		$image = imagecreatefromgif($filename);
+	    		break;
+	    }
+
+	    $exif = @exif_read_data($filename);
+
+	    if (isset($exif['Orientation'])) {
+	    	# Get orientation
+	    	$orientation = $exif['Orientation'];
+
+	    	# Manipulate image
+	    	switch ($orientation) {
+	    	    case 2:
+	    	        imageflip($image, IMG_FLIP_HORIZONTAL);
+	    	        break;
+	    	    case 3:
+	    	        $image = imagerotate($image, 180, 0);
+	    	        break;
+	    	    case 4:
+	    	        imageflip($image, IMG_FLIP_VERTICAL);
+	    	        break;
+	    	    case 5:
+	    	        $image = imagerotate($image, -90, 0);
+	    	        imageflip($image, IMG_FLIP_HORIZONTAL);
+	    	        break;
+	    	    case 6:
+	    	        $image = imagerotate($image, -90, 0);
+	    	        break;
+	    	    case 7:
+	    	        $image = imagerotate($image, 90, 0);
+	    	        imageflip($image, IMG_FLIP_HORIZONTAL);
+	    	        break;
+	    	    case 8:
+	    	        $image = imagerotate($image, 90, 0); 
+	    	        break;
+	    	}
+	    }
+
+	    
 
 	    imagecopyresized($image_p, $image, 0, 0, 0, 0, $width, $height, $orig_width, $orig_height);
 
